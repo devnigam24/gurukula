04d2c2949564bb9e8d5c20b1742d2118c4da4649
Devvrat <devnigam24@gmail.com>
2018-02-13 18:00:53 -0800

    Understanding async and await 1

---
+++ tests/unit/routes/world-timer-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:world-timer', 'Unit | Route | world timer', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
